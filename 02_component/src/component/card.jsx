b216aaf7f3c8b39e1787f99e5c1ceae5f4df8a21
Vansh Jain <vanshj9971@gmail.com>
2026-02-15 19:12:36 +0530

    Added 02_component project

---
+++ 02_component/src/component/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>Card Component</h1>
+        <p>This is a card component.</p>
+      </div>
+    </div>
+  )
+}
+
+export default card
---
+++ 02_component/src/component/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+      <div className="nav">
+        <h1>Navbar Component</h1>
+        <p>This is a navbar component.</p>
+      </div>
+    </div>
+  )
+}
+
+export default navbar
